aae5c77fbfc09badb588209accd699582620e403
Ahmet Kaya <academyakm0@gmail.com>
2025-02-05 13:58:55 +0300

    search page

--- yolcu360/lib/screens/search_page.dart
+++ yolcu360/lib/screens/search_page.dart
@@ -87,12 +87,14 @@ class _SeearchTabState extends State<SearchTab>{
       } else{
         _showResultModal(true);
       }
-      if(_fromController.text.isEmpty||
-          _toController.text.isEmpty||
-          _departureDateController.text.isEmpty||
-          _returnDateController.text.isEmpty){
+    }
+      else {
+      if (_fromController.text.isEmpty ||
+          _toController.text.isEmpty ||
+          _departureDateController.text.isEmpty ||
+          _returnDateController.text.isEmpty) {
         _showResultModal(false);
-      } else{
+      } else {
         _showResultModal(true);
       }
     }
@@ -144,20 +146,206 @@ class _SeearchTabState extends State<SearchTab>{
                               Colors.blue.shade800 :
                               Colors.white,
                             ),
-                            SizedBox(height: 16)
-
+                            SizedBox(width: 8),
+                            Text(
+                              "Araç Kiralama",
+                              style: TextStyle(
+                                color: isCarRantalSelected
+                                    ? Colors.blue.shade800
+                                    : Colors.white,
+                                fontWeight: FontWeight.bold
+                              ),
+                            )
                           ],
                         ),
                       ),
-                    )
+                    ),
+                    SizedBox(width: 16),
+                    GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          isCarRantalSelected = false;
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: !isCarRantalSelected ?
+                          Colors.white :
+                          Colors.blue.shade800,
+                          borderRadius: BorderRadius.circular(16),
+                          border: Border.all(color: Colors.white, width: 2),
+                        ),
+                        padding:
+                        EdgeInsets.symmetric(vertical: 12, horizontal: 24),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.airplane_ticket,
+                              color: !isCarRantalSelected ?
+                              Colors.blue.shade800 :
+                              Colors.white,
+                            ),
+                            SizedBox(width: 8),
+                            Text(
+                              "Uçak Bileti",
+                              style: TextStyle(
+                                  color: !isCarRantalSelected
+                                      ? Colors.blue.shade800
+                                      : Colors.white,
+                                  fontWeight: FontWeight.bold
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
                   ],
                 )
-
               ],
             ),
           ),
+          Expanded(
+              child: AnimatedSwitcher(
+                  duration: Duration(milliseconds: 300),
+                child: isCarRantalSelected
+                      ? _buildCarRentalContent()
+                      : _buildFlightTicketContent(),
+              )
+          )
         ],
       ),
+      );
+    }
+    Widget _buildCarRentalContent(){
+      return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: _pickupController,
+                decoration: InputDecoration(
+                  labelText: "Alış Yeri",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: _dropoffController,
+                decoration: InputDecoration(
+                  labelText: "Bırakış Yeri",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                      child:
+                      TextField(
+                        controller: _pickupDateController,
+                        readOnly: true,
+                        decoration: InputDecoration(
+                          labelText: "Alış Tarihi",
+                          border: OutlineInputBorder(),
+                        ),
+                        onTap: () => _selectDate(context,_pickupDateController),
+                      ),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child:
+                    TextField(
+                      controller: _dropoffDateController,
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        labelText: "Bırakış Tarihi",
+                        border: OutlineInputBorder(),
+                      ),
+                      onTap: () => _selectDate(context,_dropoffDateController),
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              ElevatedButton(
+                  onPressed: _validateAndShowResult,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange,
+                    minimumSize: Size(double.infinity, 50),
+                  ),
+                  child: Text("Araç ARA",
+                  style: TextStyle(
+                    fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+              ),
+            ],
+          ),
+      );
+    }
+    Widget _buildFlightTicketContent(){
+      return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _fromController,
+              decoration: InputDecoration(
+                labelText: "Nereden ?",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: _toController,
+              decoration: InputDecoration(
+                labelText: "Nereye ?",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            Row(
+              children: [
+                Expanded(
+                  child:
+                  TextField(
+                    controller: _departureDateController,
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      labelText: "Gidiş Tarihi",
+                      border: OutlineInputBorder(),
+                    ),
+                    onTap: () => _selectDate(context,_departureDateController),
+                  ),
+                ),
+                SizedBox(width: 8),
+                Expanded(
+                  child:
+                  TextField(
+                    controller: _returnDateController,
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      labelText: "Dönüş Tarihi",
+                      border: OutlineInputBorder(),
+                    ),
+                    onTap: () => _selectDate(context,_returnDateController),
+                  ),
+                ),
+              ],
+            ),
+            Spacer(),
+            ElevatedButton(
+              onPressed: _validateAndShowResult,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange,
+                minimumSize: Size(double.infinity, 50),
+              ),
+              child: Text("ARA",
+                style: TextStyle(
+                    fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
       );
     }
   }
