31a57fb66f58e2bf2a5c9fd2c3ad8675cc0939e3
Ahmet Kaya <academyakm0@gmail.com>
2025-02-07 11:52:47 +0300

    bottom_nav_bar

---
+++ firebasedemo/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:firebasedemo/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: HomeScreen(),
+    );
+  }
+}
