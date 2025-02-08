d0cc9124c3aeef259279b208a63cbd1d09ee8f3b
Ahmet Kaya <academyakm0@gmail.com>
2025-02-08 11:40:08 +0300

    Profile Screen

--- firebasedemo/lib/screens/profile_screen.dart
+++ firebasedemo/lib/screens/profile_screen.dart
@@ -1,12 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebasedemo/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget{
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
+    final User? currentUser = _auth.currentUser;
     return Center(
-      child: Text(
-        'Profil Sayfası',
-        style: TextStyle(fontSize: 24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Hoşgeldin, ${currentUser?.email ?? 'Misafir'}',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+              onPressed: () async{
+                await _auth.signOut();
+                Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()),
+                    (route) => false,
+                );
+              },
+              child: Text("Çıkış Yap"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red
+              ),
+          )
+        ],
       ),
     );
   }
